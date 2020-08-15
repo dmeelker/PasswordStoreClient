96e5e8787b1b3e16caa4ec92fa415455155d36eb
Dennis Meelker <dmeelker@gmail.com>
2020-08-15 19:21:22 +0200

    Basic selection

--- src/Screens/Overview/EntryTable.tsx
+++ src/Screens/Overview/EntryTable.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { PasswordEntry } from '../../Model/Model';
 import copy from 'copy-to-clipboard';
 import { FaEdit, FaCopy, FaTrashAlt } from 'react-icons/fa';
 import NotificationService from '../../Model/NotificationService';
+import { conditionalClass } from '../../RenderHelpers';
 
 export interface EntryTableProps {
   entries: Array<PasswordEntry>;
   showGroup: boolean;
-  openEntry: (entry: PasswordEntry) => any;
-  onDeleteEntry: (entry: PasswordEntry) => any;
+  //onEntrySelected: (entry: PasswordEntry) => void;
+  openEntry: (entry: PasswordEntry) => void;
+  onDeleteEntry: (entry: PasswordEntry) => void;
 }
 
 export function EntryTable(props: EntryTableProps) {
+  const [selectedEntry, setSelectedEntry] = useState<PasswordEntry>();
+
+  useEffect(() => {
+    const eventHandler = (event: KeyboardEvent) => {
+      if (selectedEntry) {
+        if(event.code === "KeyC" && event.ctrlKey) {
+          copyPassword(selectedEntry);
+        } else if(event.code === "KeyB" && event.ctrlKey) {
+          copyUsername(selectedEntry);
+        }
+      }
+    };
+
+    window.document.addEventListener("keyup", eventHandler);
+
+    return () => {
+      window.document.removeEventListener("keyup", eventHandler);
+    };
+  });
+
   function copyUsername(entry: PasswordEntry) {
     copy(entry.username);
     NotificationService.showNotification("User name copied!");
@@ -21,6 +43,11 @@ export function EntryTable(props: EntryTableProps) {
     copy(entry.password);
     NotificationService.showNotification("Password copied!");
   };
+
+  function selectEntry(entry: PasswordEntry) {
+    setSelectedEntry(entry);
+    //props.onEntrySelected(entry);
+  }
 
   return (
     <table className="w-full">
@@ -34,9 +61,9 @@ export function EntryTable(props: EntryTableProps) {
       <tbody>
         {props.entries.map((entry) => 
           <tr key={entry.id}>
-            {props.showGroup && <td className="leading-10">{entry.group.name}</td>}
-            <td className="leading-10 hover:bg-blue-100"><EntryName entry={entry}/></td>
-            <td className="leading-10">{entry.username}
+            {props.showGroup && <td className={"leading-10" + conditionalClass(selectedEntry === entry, "bg-blue-100")} onClick={() => selectEntry(entry)}>{entry.group.name}</td>}
+            <td className={"leading-10" + conditionalClass(selectedEntry === entry, "bg-blue-100")} onClick={() => selectEntry(entry)}><EntryName entry={entry}/></td>
+            <td className={"leading-10" + conditionalClass(selectedEntry === entry, "bg-blue-100")} onClick={() => selectEntry(entry)}>{entry.username}
                 <div className="float-right">
                   <button className="btn-icon" onClick={() => props.openEntry(entry)} title="Edit entry"><FaEdit/></button>
                   <button className="btn-icon" onClick={() => copyUsername(entry)} title="Copy user name"><FaCopy/></button>
