b499665247d22dd5e7855914546cd0beddf937f6
Dennis Meelker <dennis.meelker@infi.nl>
2020-08-17 20:24:29 +0200

    Basic popupmenu

--- src/Screens/Overview/GroupList.tsx
+++ src/Screens/Overview/GroupList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PasswordGroup } from '../../Model/Model';
 import { conditionalClass } from '../../Utilities/RenderHelpers';
 import { FaChevronDown, FaChevronRight } from 'react-icons/fa';
@@ -10,14 +10,41 @@ interface GroupListProps {
   onGroupSelected: (newSelection: PasswordGroup) => any;
 }
 
+interface PopupState {
+  visible: boolean;
+  x: number;
+  y: number;
+}
+
 export function GroupList(props: GroupListProps) {
+  const [popupState, setPopupState] = useState<PopupState>();
+  let popupLocation = {x: 0, y: 0};
+
+  function onPopupRequested(group: PasswordGroup, x: number, y: number) {
+    setPopupState({visible: true, x, y});
+    console.log(popupState);
+  }
+
+  let popupMenu;
+
+  if(popupState?.visible) {
+    popupMenu = <div className="fixed inset-0" onClick={() => setPopupState(undefined)}>
+      <div className="absolute bg-white shadow p-2" style={{left: popupState.x, top: popupState.y}}>
+        <button className="block">Rename</button>
+        <button className="block">Delete</button>
+      </div>
+    </div>
+  }
+
   return (<div className="group-list overflow-x-hidden">
       <GroupNode 
         key={props.groups.id}
         group={props.groups}
         selectedGroup={props.selectedGroup} 
         onGroupSelected={props.onGroupSelected}
+        onPopupMenu={onPopupRequested}
       />
+      {popupMenu}
   </div>);
 }
 
@@ -25,6 +52,7 @@ interface GroupNodeProps {
   group: PasswordGroup;
   selectedGroup: PasswordGroup;
   onGroupSelected: (newSelection: PasswordGroup) => any;
+  onPopupMenu: (group: PasswordGroup, x: number, y: number) => void;
 }
 
 function GroupNode(props: GroupNodeProps) {
@@ -71,6 +99,14 @@ function GroupNode(props: GroupNodeProps) {
     EntryService.moveGroup(data, group.id)
   }
 
+  function onContextMenu(event: React.MouseEvent) {
+    if(event.button === 2) {
+      props.onPopupMenu(props.group, event.pageX, event.pageY);
+      event.preventDefault();
+      return false;
+    }
+  }
+
   return (
     <div>
       <div className="leading-8 whitespace-no-wrap">
@@ -79,9 +115,17 @@ function GroupNode(props: GroupNodeProps) {
             (collapsed ? <FaChevronRight/> : <FaChevronDown/>)
           }
         </button>
-        <button draggable="true" onDragStart={onDragStart} onDragEnter={onDragEnter} onDragOver={onDragOver} onDragLeave={onDragExit} onDrop={(e) => onDrop(e, props.group)} className={"flex-1 px-2 text-left rounded hover:bg-green-200 focus:bg-green-200 focus:outline-none" + conditionalClass(props.selectedGroup === props.group, "bg-green-200")} 
+        <button 
+          className={"flex-1 px-2 text-left rounded hover:bg-green-200 focus:bg-green-200 focus:outline-none" + conditionalClass(props.selectedGroup === props.group, "bg-green-200")} 
+          draggable="true" 
+          onDragStart={onDragStart} 
+          onDragEnter={onDragEnter} 
+          onDragOver={onDragOver} 
+          onDragLeave={onDragExit} 
+          onDrop={(e) => onDrop(e, props.group)} 
           onClick={groupSelected} 
-          onDoubleClick={toggleCollapse}>{props.group.name}</button>
+          onDoubleClick={toggleCollapse}
+          onContextMenu={onContextMenu}>{props.group.name}</button>
       </div>
       <div className={conditionalClass(collapsed, "hidden") + "pl-4"}>
         {props.group.groups.map((child) =>
@@ -89,7 +133,8 @@ function GroupNode(props: GroupNodeProps) {
             key={child.id} 
             group={child} 
             selectedGroup={props.selectedGroup}
-            onGroupSelected={props.onGroupSelected}/>)
+            onGroupSelected={props.onGroupSelected}
+            onPopupMenu={props.onPopupMenu}/>)
         }
         </div>
     </div>)
